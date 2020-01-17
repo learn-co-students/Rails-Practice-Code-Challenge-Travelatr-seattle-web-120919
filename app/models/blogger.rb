740bea53a3cccf3a33b544fe32b4195bd659904f
Azhang9328 <azhang9328@gmail.com>
2020-01-17 11:49:01 -0800

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,26 @@
 class Blogger < ApplicationRecord
+    has_many :posts 
+    has_many :destinations, through: :posts
 
+    validates :name, uniqueness: true
+    validates :age, numericality: {greater_than: 0}
+    validates :bio, length: {minimum: 30}
+
+    def total_likes
+        postlikes = self.posts.map do |post|
+            post.likes
+        end
+        
+        postlikes.reduce(0) do |total, likes|
+            total += likes 
+        end
+    end
+
+    def most_liked
+        postsarray = self.posts.sort_by do |post|
+            post.likes
+        end
+
+        postsarray.last
+    end
 end
